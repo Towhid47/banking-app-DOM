2db0d8c89a3fcdaae9fabf6ec1db92712d3adfd3
Towhid47 <mdtowhidurrahman47@gmail.com>
2026-05-11 17:41:28 +0600

    Login System upgraded with dynamic users data

---
+++ data.js
@@ -0,0 +1,22 @@
+export let usersData = [
+    {
+        username: "md selim",
+        pin: "1234",
+        mobileNumber: "01710728283",
+        balance: 5000
+    },
+    {
+        username: "md tariq",
+        pin: "4321",
+        mobileNumber: "01703222887",
+        balance: 5600
+    },
+    {
+        username: "Nazifa Akter",
+        pin: "5678",
+        mobileNumber: "01803222887",
+        balance: 8000
+    },
+
+]; 
+
--- login.js
+++ login.js
@@ -1,21 +1,25 @@
-const userData = {mobileNumber: "01710728283", pin: "1234"};
+import { usersData } from "./data.js"; 
 
 const loginForm = document.querySelector('#login-form'); 
 
 loginForm.addEventListener("submit", function(event){
      event.preventDefault();
 
-     const mobileNumber = document.querySelector('#mobile-number').value;
-     const pin = document.querySelector('#pin').value; 
+     const mobileNumberInput = document.querySelector('#mobile-number').value;
+     const pinNumberInput = document.querySelector('#pin').value; 
 
-        // Validate the inputs
-        if(mobileNumber === userData.mobileNumber && pin === userData.pin){
-            console.log(window.location.href = "/main.html");
-        }
-        else if(mobileNumber === "" || pin === ""){
-            alert("Please fill in both fields.");
+     //get user data according to input
+     let userData = usersData.find(user => user.mobileNumber === mobileNumberInput && user.pin === pinNumberInput);
+
+    // Navigate to main page if user data is found, otherwise show an error message 
+        if(userData){
+            window.location.href = "/main.html";
         }
         else{
             alert("Invalid mobile number or PIN. Please try again.");
         }
 });
+
+
+
+
